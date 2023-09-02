0252432b48588d5c74bf4ff7949a3a69c6268e07
priyanshu <test@gmail.com>
2023-09-02 14:12:10 +0530

    Video 74

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adibas shoes 50 INR</h2>
+        <button className='btn btn-primary mx-2'>-</button>
+        Add this item to cart
+        <button className='btn btn-primary mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
